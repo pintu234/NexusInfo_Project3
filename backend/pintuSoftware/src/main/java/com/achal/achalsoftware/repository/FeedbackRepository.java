fc989caf1eb3ee0f260d24b8cf48b158a5cea155
pintu234 <137603142+pintu234@users.noreply.github.com>
2024-07-18 09:59:15 +0530

    Add files via upload

---
+++ backend/pintuSoftware/src/main/java/com/achal/achalsoftware/repository/FeedbackRepository.java
@@ -0,0 +1,9 @@
+package com.achal.achalsoftware.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.achal.achalsoftware.model.FeedBack;
+
+public interface FeedbackRepository  extends JpaRepository<FeedBack, Long>{
+
+}
